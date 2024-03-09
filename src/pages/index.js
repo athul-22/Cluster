f04cee22453b8004b8b8657b61bae55c8739d4fa
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2024-03-09 15:16:43 +0530

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Login from "./Login";
-import Home from "./Home";
-import Register from "./Register";
-import Profile from "./Profile";
-import ResetPassword from "./ResetPassword";
-import Admin from './Admin';
-
-export { Login, Home, Register, Profile, ResetPassword ,Admin};
